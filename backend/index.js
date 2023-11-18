c6fa36022262bbbd0330b30e09d2e776620895aa
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-11-18 23:19:41 +0530

    signed in

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const signInUp = require("./controller/SignInUp");
+const signInUp = require("./controller/signInUpRoute");
+const cors = require("cors");
 
 app.set("view engine", "ejs")
 app.use('/controller', express.static('public/controller'));
+app.use(cors());
+app.use(express.json());
 
 app.use("/routes", router);
-
-
-router.get("/", (req, res) => {
-    res.render('login', {
-        utils: signInUp
-    }); // Don't include the file extension or folder path
-})
+app.use("/signinup", signInUp);
 
 app.listen(4000, () => {
     console.log("Server started at port 4000");
--- frontend/src/components/signup-signin/Loginsignup.jsx
+++ frontend/src/components/signup-signin/Loginsignup.jsx
@@ -3,45 +3,89 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faLock, faCalendarAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
 import './Loginsignup.css';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 const Loginsignup = () => {
     const [action, setAction] = useState("Sign up");
 
+    const [errorMessage, setErrorMessage] = useState(null);
+    const navigate = useNavigate();
+
+    const [userData, setUserData] = useState({
+        uname: '',
+        email: '',
+        password: '',
+        age: '',
+        phoneNum: ''
+      });
+
+      const handleChange = (e) => {
+        setUserData({
+          ...userData,
+          [e.target.name]: e.target.value,
+        });
+      };
+
+      const handleSignup = async () => {
+        try {
+          const response = await axios.post('http://localhost:4000/signinup/signup', userData);
+          console.log(response);
+          if(typeof response.data === 'string' ){
+            console.log("2");
+            setErrorMessage('User already exists. Please choose a different email.');
+            setTimeout(() => {
+                setErrorMessage(null);
+              }, 1500);
+          }
+          else {
+            localStorage.setItem('token', response);
+            navigate("./components/TodoList");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
     return (
         <div>
             <div className="container login-signup">
+                {errorMessage && (
+                    <div className="alert alert-warning" role="alert">
+                    {errorMessage}
+                    </div>
+                )}
                 <div className="header-login">
                     <div className="text">{action}</div>
                     <div className="underline"></div>
                 </div>
-
                 <div className="inputs">
                     {action === 'Login' ? <div></div> : <div className="input">
                         <FontAwesomeIcon icon={faUser} />
-                        <input type="text" placeholder=' Name' />
+                        <input className='inputTaker' type="text" placeholder=' Name' name='uname' value={userData.uname} onChange={handleChange} />
                     </div>}
 
                     <div className="input">
                         <FontAwesomeIcon icon={faEnvelope} />
-                        <input type="email" placeholder=' Email' />
+                        <input className='inputTaker' type="email" placeholder=' Email' name="email" value={userData.email} onChange={handleChange} />
                     </div>
 
                     <div className="input">
                         <FontAwesomeIcon icon={faLock} />
-                        <input type="password" placeholder=' Password' />
+                        <input className='inputTaker' type="password" placeholder=' Password' name="password" value={userData.password} onChange={handleChange} />
                     </div>
 
                     {action === 'Login' ? null : (
                         <div className="input">
                             <FontAwesomeIcon icon={faCalendarAlt} />
-                            <input type="text" placeholder=' Age' />
+                            <input className='inputTaker' type="number" placeholder=' Age' name="age" value={userData.age} onChange={handleChange} />
                         </div>
                     )}
 
                     {action === 'Login' ? null : (
                         <div className="input">
                             <FontAwesomeIcon icon={faPhone} />
-                            <input type="tel" placeholder=' Phone Number' />
+                            <input className='inputTaker' type="tel" placeholder=' Phone Number' name="phoneNum" value={userData.phoneNum} onChange={handleChange} />
                         </div>
                     )}
 
@@ -49,8 +93,12 @@ const Loginsignup = () => {
                 {action === "Sign up" ? <div></div> : <div className="forgot-password">Lost password? <span>Click Here!</span></div>}
 
                 <div className="submit-container">
-                    <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction('Sign up') }}>Signup</div>
-                    <div className={action === "Sign up" ? "submit gray" : "submit"} onClick={() => { setAction('Login') }}>Login</div>
+                    <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { 
+                            action === 'Sign up' ? handleSignup() : setAction('Sign up') 
+                        }}>Signup</div>
+                    <div className={action === "Sign up" ? "submit gray" : "submit"} onClick={() => { 
+                            action === 'Login' ? console.log("login") : setAction('Login') 
+                        }}>Login</div>
                 </div>
             </div>
         </div>
